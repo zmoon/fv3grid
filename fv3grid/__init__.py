25a451d517df55df23009dd77c9ad3bb394da739
Barry Baker <bbakernoaa@users.noreply.github.com>
2019-03-22 12:12:41 -0400

    Update __init__.py
    Add function to retrieve file easily and return xarray object for given resolution and tile

--- fv3grid/__init__.py
+++ fv3grid/__init__.py
@@ -1,2 +1,14 @@
 import os
+import xarray as xr
+
+def get_fv3_grid(res='C384',tile=1):
+    cdir = os.path.abspath(__file__)[:-11]
+    gridname = "{}_grid_spec.tile{}.nc".format(res,str(int(tile)))
+    fname = "{}{}".format(cdir,gridname)
+    o = xr.open_dataset(fname)
+    o = o.rename({'grid_xt':'x',
+                  'grid_yt':'y',
+                  'grid_latt':'latitude',
+                  'grid_lont':'longitude'})
+    return o.squeeze()
 
