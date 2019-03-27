86f2fa0a6861754de5db45afd4891c90641f006f
Barry Baker <bbakernoaa@users.noreply.github.com>
2019-03-27 09:42:07 -0400

    Update __init__.py
    renaming of boundary coordinates for future use with xESMF conservative resampling

--- fv3grid/__init__.py
+++ fv3grid/__init__.py
@@ -9,6 +9,10 @@ def get_fv3_grid(res='C384',tile=1):
     o = o.rename({'grid_xt':'x',
                   'grid_yt':'y',
                   'grid_latt':'latitude',
-                  'grid_lont':'longitude'})
-    return o.squeeze().set_coords(['latitude','longitude'])
+                  'grid_lont':'longitude',
+                  'grid_lat':'lat_b',
+                  'grid_lon':'lon_b',
+                  'grid_x':'x_b',
+                  'grid_y':'y_b'}).drop('time')
+    return o.squeeze().set_coords(['latitude','longitude','lat_b','lon_b'])
 
